f67f1770ab0fe027a69086d8cd9ebd9e02f6b1e1
DophBone <alvarovera33@gmail.com>
2022-05-27 13:24:33 +0200

    PrimerComit

---
+++ src/mainP/MainClass.java
@@ -0,0 +1,16 @@
+package mainP;
+
+public class MainClass {
+
+	static boolean facha = false;
+
+	public static void main(String[] args) {
+
+		if (facha) {
+			System.out.println("Viva españa");
+		} else
+			System.out.println("No!");
+
+	}
+
+}
